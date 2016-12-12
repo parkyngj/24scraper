d1956ff0c9a97627b4959c151249c7adbd954caa
Young Ju Park <parkyngj@gmail.com>
2016-12-11 19:51:43 -0800

    successfully scrape all combinations starting from 1-2

--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,38 @@
-require 'pp'
+require 'csv'
 require_relative 'urltonokogiri'
 
 url1_2 = "http://www.gottfriedville.net/games/24/index.shtml"
 
 url1_2_noko_page = UrlToNokogiri.convert_url_to_ng(url1_2)
 
-combos_str = url1_2_noko_page.children[1].children[1].children.last.children.children.children.children.text
+combos1_2_str = url1_2_noko_page.children[1].children[1].children.last.children.children.children.children.text
 
-combos_ary = combos_str.split(" ")
+combos1_2_ary = combos1_2_str.split(" ")
 
-combos_ary.each_with_index.map do |char,index|
+combos1_2_ary.each_with_index.map do |char,index|
   if char != "nope"
     next
   else
     for idx in (index-5..index) do
-      combos_ary[idx] = nil
+      combos1_2_ary[idx] = nil
     end
   end
 end
 
-combos_ary.compact!.reject!{ |char| char == ":" }
+combos1_2_ary.compact!.reject!{ |char| char == ":" }
 
 i = 0
-valid_combos = Hash.new
+valid1_2_combos = Hash.new
 
-until i == combos_ary.length
-  hand = combos_ary[i..i+3].join(" ")
-  soln = combos_ary[i+4]
-  valid_combos[hand] = soln
+until i == combos1_2_ary.length
+  hand = combos1_2_ary[i..i+3].join(" ")
+  soln = combos1_2_ary[i+4]
+  valid1_2_combos[hand] = soln
   i += 5
 end
 
-p valid_combos
-
-# CSV.open("hands.csv", "a+") do |csv|
-#
-# end
+CSV.open("hands.csv", "a+") do |csv|
+  valid1_2_combos.each do |hand,soln|
+    csv << [hand, soln]
+  end
+end
