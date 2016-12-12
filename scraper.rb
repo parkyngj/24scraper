a84880ea9a8affdc2850f2b94c9967c624855e81
Young Ju Park <parkyngj@gmail.com>
2016-12-11 19:59:41 -0800

    add urls for 3-4 and 5-13 hand combinations

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,20 @@
-require 'csv'
 require_relative 'urltonokogiri'
+require 'csv'
 
 url1_2 = "http://www.gottfriedville.net/games/24/index.shtml"
+url3_4 = "http://www.gottfriedville.net/games/24/indexb.shtml"
+url5_13 = "http://www.gottfriedville.net/games/24/indexc.shtml"
 
 url1_2_noko_page = UrlToNokogiri.convert_url_to_ng(url1_2)
+url3_4_noko_page = UrlToNokogiri.convert_url_to_ng(url3_4)
+url5_13_noko_page = UrlToNokogiri.convert_url_to_ng(url5_13)
+
 
 combos1_2_str = url1_2_noko_page.children[1].children[1].children.last.children.children.children.children.text
+combos3_4_str = url3_4_noko_page.children[1].children[1].children.last.children.children.children.children.text
+combos5_13_str = url5_13_noko_page.children[1].children[1].children.last.children.children.children.children.text
+
+combo_strs = [combos1_2_str, combos3_4_str, combos5_13_str]
 
 combos1_2_ary = combos1_2_str.split(" ")
 
