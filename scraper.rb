8d70ffebcb381a64f83e791aa1bd97c4052fba50
Young Ju Park <parkyngj@gmail.com>
2016-12-11 18:59:01 -0800

    adds whitespace cleaner

---
+++ scraper.rb
@@ -0,0 +1,4 @@
+require 'net/http'
+require 'nokogiri'
+
+url1_2 = "http://www.gottfriedville.net/games/24/index.shtml"
---
+++ urltonokogiri.rb
@@ -0,0 +1,4 @@
+require 'net/http'
+require 'uri'
+require 'nokogiri'
+require_relative 'html_whitespace_cleaner'
